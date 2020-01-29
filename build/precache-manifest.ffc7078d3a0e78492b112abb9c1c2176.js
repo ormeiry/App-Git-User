de7cee4f7cc89652f20e05aa742b26f2b4f55a38
Or Meiry <ormeiry2411@gmail.com>
2020-01-30 00:12:10 +0200

    Added the build folder manually

---
+++ build/precache-manifest.ffc7078d3a0e78492b112abb9c1c2176.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef1eb615c33f934d34d95d4715ae39b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0c34317b6cfa2dc164b",
+    "url": "/static/css/main.4411de65.chunk.css"
+  },
+  {
+    "revision": "046f5a1ad08d9f4c997b",
+    "url": "/static/js/2.7048bb8d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.7048bb8d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c0c34317b6cfa2dc164b",
+    "url": "/static/js/main.a3cbccca.chunk.js"
+  },
+  {
+    "revision": "e7be2421d4d75481ec56",
+    "url": "/static/js/runtime-main.2a529da5.js"
+  }
+]);
